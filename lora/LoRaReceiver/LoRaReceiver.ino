cb6f6fd7f985a659159d4d349e3c90327a11ba03
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2024-07-25 17:03:16 +0200

    feat: LoRa receiver with hono forwarding

--- lora/LoRaReceiver/LoRaReceiver.ino
+++ lora/LoRaReceiver/LoRaReceiver.ino
@@ -1,22 +1,84 @@
-// Only supports SX1276/SX1278
-#include "utils.h"
-#include "LoRaClient.h"
+/**
+ * @author    Manuel Andruccioli
+ * @file      LoRaReceiver.ino
+ */
 
-void setup() {
-    setupLoRa();
+#include "LoRaClient.h"
+#include "WiFiHonoClient.h"
+
+#define MAX_TIME_TO_WAIT_FOR_LORA_PACKET_MS 10000
+
+void handlePacketFromLoRa(String packet) {
+    Serial.println("Receive from LoRa: " + packet);
+    if (u8g2) {
+        u8g2->clearBuffer();
+        u8g2->drawStr(0, 12, "Receive from LoRa:");
+        u8g2->drawStr(0, 30, packet.c_str());
+        u8g2->sendBuffer();
+    }
+
+    sendPacketToHono(packet);
 }
 
-void loop() {
+void sendPacketToHono(String packet) {
+    int httpResponseCode = sendTelemetryToHono(packet);
 
-    // // try to parse packet
+    if (httpResponseCode >= 200 && httpResponseCode <= 299) {
+        Serial.println("Telemetry sent to Hono successfully");
+        if (u8g2) {
+            u8g2->drawStr(0, 48, "Hono OK!");
+            u8g2->sendBuffer();
+        }
+    } else {
+        Serial.println("Failed to send telemetry to Hono");
+        if (u8g2) {
+            u8g2->drawStr(0, 48, "Hono FAIL!");
+            u8g2->sendBuffer();
+        }
+    }
+}
+
+int forecastMeasure() {
+    return random(0, 41); // just random values for the sake of the example
+}
+
+void handleForecasting() {
+    int measure = forecastMeasure();
+    Serial.println("No packets received for more than 5 seconds");
+    if (u8g2) {
+        u8g2->clearBuffer();
+        u8g2->drawStr(0, 12, "No LoRa packets");
+        String msg = "Forecast: " + String(measure);
+        u8g2->drawStr(0, 30, msg.c_str());
+        u8g2->sendBuffer();
+    }
+
+    String packet = "{\"measure\": " + String(measure) + ", \"forecast\": \"true\"}";
+    sendPacketToHono(packet);
+}
+
+void setup() {
+    Serial.begin(115200);
+    setupLoRa();
+    setupWiFi();
+}
+
+unsigned long lastPacketTime = 0;
+
+void loop() {
+    // try to parse packet
     String packet = parsePacketPayload();
     if (packet != "") {
-        if (u8g2) {
-            u8g2->clearBuffer();
-            char buf[256];
-            u8g2->drawStr(0, 12, "Received OK!");
-            u8g2->drawStr(0, 26, packet.c_str());
-            u8g2->sendBuffer();
+        lastPacketTime = millis();
+        handlePacketFromLoRa(packet);
+    } else {
+        unsigned long currentTime = millis();
+        unsigned long elapsedTimeBetweenLastPacket = currentTime - lastPacketTime;
+
+        // Forecasting measure if not receiving
+        if (elapsedTimeBetweenLastPacket > MAX_TIME_TO_WAIT_FOR_LORA_PACKET_MS) {
+            lastPacketTime = millis();
+            handleForecasting();
         }
     }
 }
