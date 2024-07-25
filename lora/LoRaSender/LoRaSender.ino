6ad6173bdb406700a2121b6bf3dd76dc250c5575
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2024-07-25 16:57:39 +0200

    feat: LoRa Sender

--- lora/LoRaSender/LoRaSender.ino
+++ lora/LoRaSender/LoRaSender.ino
@@ -1,8 +1,14 @@
-// Only supports SX1276/SX1278
+/**
+ * @author    Manuel Andruccioli
+ * @file      LoRaSender.ino
+ */
+
 #include "utils.h"
 #include "LoRaClient.h"
 
-int counter = 0;
+int makeMeasurement() {
+    return random(20, 31); // just random values for the sake of the example
+}
 
 void setup() {
     setupLoRa();
@@ -10,19 +16,24 @@ void setup() {
 
 void loop()
 {
-    Serial.print("Sending packet: ");
-    Serial.println(counter);
-
-    sendPacket(String(counter));
+    int measure = makeMeasurement();
+    String packet = "{\"measure\": " + String(measure) + "}";
+    Serial.println("Sending with LoRa: " + packet);
 
     if (u8g2) {
-        char buf[256];
         u8g2->clearBuffer();
-        u8g2->drawStr(0, 12, "Transmitting: OK!");
-        snprintf(buf, sizeof(buf), "Sending: %d", counter);
-        u8g2->drawStr(0, 30, buf);
+        u8g2->drawStr(0, 12, "Sending with LoRa:");
+        u8g2->drawStr(0, 30, packet.c_str());
         u8g2->sendBuffer();
     }
-    counter++;
-    delay(500);
+
+    sendPacket(packet);
+    Serial.println("Sending: " + packet);
+
+    if (u8g2) {
+        u8g2->drawStr(0, 48, "Message Sent!");
+        u8g2->sendBuffer();
+    }
+
+    delay(1000);
 }
