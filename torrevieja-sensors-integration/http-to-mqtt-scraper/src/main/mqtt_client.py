d1eca147ceb90c24f5c6fc6a11265a3f53ec7c53
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2024-08-13 21:23:57 +0200

    chore: add logger for mqtt client

--- torrevieja-sensors-integration/http-to-mqtt-scraper/src/main/mqtt_client.py
+++ torrevieja-sensors-integration/http-to-mqtt-scraper/src/main/mqtt_client.py
@@ -1,11 +1,14 @@
 from paho.mqtt import client as mqtt_client
+from logging import getLogger
+
+logger = getLogger(__name__)
 
 def connect_mqtt(broker: str, port: int) -> mqtt_client.Client:
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
-            print("Connected to MQTT Broker!")
+            logger.info("Connected to MQTT Broker!")
         else:
-            print("Failed to connect, return code %d\n", rc)
+            logger.error("Failed to connect, return code %d\n", rc)
 
     client = mqtt_client.Client()
     client.on_connect = on_connect
