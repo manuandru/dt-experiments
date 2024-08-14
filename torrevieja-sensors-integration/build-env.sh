ec0906c9581b320f5995674a8470bd688a9fde49
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2024-08-14 14:29:31 +0200

    feat: config file for list of thing to provision and their wot declaration

--- torrevieja-sensors-integration/build-env.sh
+++ torrevieja-sensors-integration/build-env.sh
@@ -13,7 +13,12 @@ if ! command -v envsubst &> /dev/null; then
     exit 1
 fi
 
+# Replace all environment variables in for thing-directory deployment
 for file in $(ls thing-directory/root/*.jsonld); do
     THING_DIRECTORY=$THING_DIRECTORY envsubst < $file > $file.tmp
     mv $file.tmp $file
 done
+
+# Replace all environment variables in for thing-provisioner deployment
+file=ditto-thing-provisioner/thing-description.json
+THING_DIRECTORY=$THING_DIRECTORY envsubst < $file.template > $file
