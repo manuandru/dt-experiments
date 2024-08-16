566443b539a15f0aacad50b0c45dcab5d9b20643
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2024-08-16 13:23:18 +0200

    feat: add javascript mappers for ditto-mqtt connection

--- torrevieja-sensors-integration/ditto-provisioner/connection/connection-provisioner.sh
+++ torrevieja-sensors-integration/ditto-provisioner/connection/connection-provisioner.sh
@@ -6,19 +6,34 @@ DITTO_DEVOPS_USERNAME=${DITTO_DEVOPS_USERNAME:-"devops"}
 DITTO_DEVOPS_PASSWORD=${DITTO_DEVOPS_PASSWORD:-"foobar"}
 MQTT_BROKER=${MQTT_BROKER:-"tcp://mqtt-broker:1883"}
 
+JAVASCRIPT_MAPPER=$(jq -Rs . < javascript-mapper.js)
+
 curl \
     -X POST $DITTO_ENDPOINT \
     -u $DITTO_DEVOPS_USERNAME:$DITTO_DEVOPS_PASSWORD \
     -H 'Content-Type: application/json' \
-    -d '{
+    -d @- <<EOF
+{
     "name": "MQTT Connection",
     "connectionType": "mqtt",
     "connectionStatus": "open",
     "failoverEnabled": true,
-    "uri": "'$MQTT_BROKER'",
+    "uri": "$MQTT_BROKER",
     "sources": [{
         "addresses": ["sensors/#"],
         "authorizationContext": ["nginx:ditto"],
-        "qos": 1
-    }]
-}'
+        "qos": 1,
+        "payloadMapping": ["javascript"]
+    }],
+    "mappingDefinitions": {
+        "javascript": {
+            "mappingEngine": "JavaScript",
+            "options": {
+                "incomingScript": $JAVASCRIPT_MAPPER,
+                "loadBytebufferJS": "false",
+                "loadLongJS": "false"
+            }
+        }
+    }
+}
+EOF
